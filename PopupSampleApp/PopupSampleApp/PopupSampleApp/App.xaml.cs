a085c741af0f81e10fc5aa360438f4f1e81c7008
Shingo Takahashi <takahashi@be-org.co.jp>
2017-02-18 22:34:00 +0900

    ポップアップもどきのサンプル

---
+++ PopupSampleApp/PopupSampleApp/PopupSampleApp/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Unity;
+using PopupSampleApp.Views;
+
+namespace PopupSampleApp
+{
+	public partial class App : PrismApplication
+	{
+		public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+		protected override void OnInitialized()
+		{
+			InitializeComponent();
+
+			NavigationService.NavigateAsync("PopupSamplePage?title=Hello%20from%20Xamarin.Forms");
+		}
+
+		protected override void RegisterTypes()
+		{
+			Container.RegisterTypeForNavigation<PopupSamplePage>();
+		}
+	}
+}
---
+++ PopupSampleApp/PopupSampleApp/PopupSampleApp/Views/PopupSamplePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace PopupSampleApp.Views
+{
+	public partial class PopupSamplePage : ContentPage
+	{
+		public PopupSamplePage()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, EventArgs e)
+		{
+			stackLayout.IsVisible = true;
+			popup.IsVisible = true;
+		}
+
+		private void stackLayout_Tapped(object sender, TappedEventArgs e)
+		{
+			stackLayout.IsVisible = false;
+			popup.IsVisible = false;
+		}
+	}
+}
